9285556039c4dc0b52dfe8c9340e0ef4b4d40e1a
PhamPhien-gif <toilaphien2302@gmail.com>
2026-09-25 14:45:34 +0700

    edit search param

--- my-app/src/features/users/services/useService.js
+++ my-app/src/features/users/services/useService.js
@@ -1,11 +1,11 @@
 import axiosClient from "@/services/axiosClient";
 export const userService = {
   createTeacher: (payload) => axiosClient.post("/teachers", payload),
-  getAllUsers: ({ search = "", role, pageNumber = 1, pageSize = 10 } = {}) =>
+  getAllUsers: ({ name = "", email = "", role, pageNumber = 1, pageSize = 10 } = {}) =>
     axiosClient.get("/admin/users", {
       params: {
-        Name: search,
-        Email: search,
+        Name: name || undefined,
+        Email: email || undefined,
         Role: role,
         PageNumber: pageNumber,
         PageSize: pageSize,
